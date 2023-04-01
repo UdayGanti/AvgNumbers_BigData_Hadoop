d4462192314b34f5a5082007cee18891b047bcce
UdayGanti <udayganti22@gmail.com>
2023-04-01 10:46:15 -0400

    Adding 4 files

---
+++ mapper.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""mapper.py"""
+
+import sys
+
+for line in sys.stdin:
+    # Split the line into fields
+    fields = line.strip().split()
+    
+    # Convert the field to a float and emit it with a count of 1
+    for field in fields:
+        print("{0}\t{1}\t{2}".format("total", float(field), 1))
---
+++ reducer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""reducer.py"""
+
+import sys
+
+total = 0
+count = 0
+
+for line in sys.stdin:
+    # Split the line into fields
+    key, value, cnt = line.strip().split("\t")
+    
+    # Update the sum and count
+    total += float(value)
+    count += int(cnt)
+
+# Calculate the mean
+average = total / count
+
+print(average)
